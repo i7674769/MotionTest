dc15108a0f7407c734a5063f9201c386147f11d7
Rebecca Hughes (i7674769) <i7674769@mac42943.student.bournemouth.ac.uk>
2017-11-16 14:49:42 +0000

    Initial Commit

---
+++ MotionTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MotionTest
+//
+//  Created by Rebecca Hughes (i7674769) on 16/11/2017.
+//  Copyright © 2017 Rebecca Hughes (i7674769). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
